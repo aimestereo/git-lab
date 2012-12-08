77a0e3116b6a93be70e1638f4def38ba2ddb48d8
Valentin Kuznetsov <vvkuznetsov@hotmail.com>
2012-12-08 17:22:32 +0400

    mission complete - a evil's killer is ready.

--- killEvil.py
+++ killEvil.py
@@ -1,1 +1,19 @@
-print('I\ll help you... soon...')
+import os
+import psutil
+
+print("I\'ll help you... soon...")
+
+#PROCNAME = "python.exe"
+PROCNAME = "Python"
+
+def isEvil(arr):
+	for item in arr:
+		if item.find("Evil") != -1:
+			return True		#EVIL!!!
+
+	return False	#it's not Evil
+
+for proc in psutil.process_iter():
+    if proc.name == PROCNAME and isEvil(proc.cmdline) and proc.pid != os.getpid():
+        print "I found child of evil - ", proc.cmdline
+        proc.kill()
